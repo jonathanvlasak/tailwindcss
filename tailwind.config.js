dd273ea2a700d8c7b945b648f72b79da2b793683
Jon <jonathan.vlasak@gmail.com>
2021-06-24 11:42:01 -0700

    Updated with new layout/concept

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,65 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
-    debugScreens: {
-      position: ['top', 'left'],
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            img: { 
+                
+            },
+            h1: {
+              fontSize: '1.5rem',
+              color: '#3C6661',
+              fontWeight: '600',
+              lineHeight: '1'
+            },
+            h3: {
+              color: '#3C6661',
+            },
+          },
+        },
+      },
+      colors: {
+        green: {
+          850: '#3C6661',
+          650: '#84997A',
+          550: '#9DB9BB'
+        },
+        blue: {
+          550: '#4793ad'
+        },
+      },
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        sans : ['Inter', 'sans-serif'],
+        header: ['serif']
+      },
     },
-    extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [
-    require('tailwindcss-debug-screens'),
+    require('@tailwindcss/typography'),
+    plugin(function({ addBase, theme }) {
+      addBase({
+        'li a': { 
+          color: theme('colors.green.800'),
+          fontSize: theme('fontSize.xl'),
+          },
+        'h1': { 
+            color: theme('colors.blue.550'),
+            fontSize: theme('fontSize.2xl'),
+            fontWeight: theme('fontWeight.semibold'),
+          },
+          'p': { 
+            fontSize: theme('fontSize.sm'),
+          },
+      })
+    })
   ],
 }
